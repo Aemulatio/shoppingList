153124405ad1977b8b885668ee6b847517c055e5
Stepan S <dumn2011@gmail.com>
2023-08-26 21:53:55 +0300

    add: component, that adds new value to the context store

---
+++ src/components/NewItem/NewItem.tsx
@@ -0,0 +1,41 @@
+import styles from "./NewItem.module.css";
+import React, { useId } from "react";
+import { useItemsContext } from "../../contexts/ItemsContext.tsx";
+
+export const NewItem = () => {
+  const { dispatch } = useItemsContext();
+  const id = useId();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!(e.target as HTMLFormElement).childNodes.length) return;
+
+    dispatch({
+      type: "ADD",
+      payload: {
+        id,
+        text: (
+          (e.target as HTMLFormElement).childNodes[0]
+            .firstChild as HTMLInputElement
+        )?.value,
+        done: false,
+      },
+    });
+
+    (
+      (e.target as HTMLFormElement).childNodes[0].firstChild as HTMLInputElement
+    ).value = "";
+  };
+
+  return (
+    <form className={styles.root} onSubmit={handleSubmit}>
+      <label title={"Новый элемент"}>
+        <input
+          type="text"
+          placeholder={"Добавить"}
+          name={"item"}
+          className={styles.input}
+        />
+      </label>
+    </form>
+  );
+};
